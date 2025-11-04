9687fd94b9d1a476c5cb1a8c0184c2856753ce5d
PAPATIRI <syariftaufik11@gmail.com>
2025-11-04 11:54:30 +0700

    fix(bugs): setup multipage web app di vite js

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,14 @@
 import { defineConfig } from "vite";
-import path from "path";
-import { resolve } from "path";
+import { fileURLToPath } from "node:url";
+import { dirname, resolve } from "node:path";
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
 export default defineConfig({
-  base: "./",
-  resolve: {
-    alias: {
-      "@assets": path.resolve(__dirname, "./public"),
-    },
-  },
   build: {
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
-        karir: resolve(__dirname, "karir.html"),
+        karir: resolve(__dirname, "karir/index.html"),
       },
     },
   },
